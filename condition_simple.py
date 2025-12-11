446a7359a711d91dd5608204a32429d46ea13c4b
JImambo <imambojonathan@gmail.com>
2025-12-11 15:07:34 +0100

    commit condition_simple

--- condition_simple.py
+++ condition_simple.py
@@ -1,6 +1,15 @@
+continuer = "oui"
 
-nombre: int = int(input("Saisissez un nombre"))
+while continuer.lower() == "oui":
+    nombre: int = int(input("Saisissez un nombre: "))
 
-if nombre > 0:
-    print("Nombre positif")
-elif nombre <
+    if nombre > 0:
+        print("Nombre positif")
+    elif nombre < 0:
+        print("Nombre negatif")
+    else:
+        print("Zero")
+        
+    continuer = input("Voulez-vous continuer ? (oui/non) : ")
+    
+print("Programme terminé")
