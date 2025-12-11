f72981fe860f336c2be8095c567a468f676a2cd5
JImambo <imambojonathan@gmail.com>
2025-12-11 15:42:49 +0100

    commit condition_simple avec gestion des erreurs

--- condition_simple.py
+++ condition_simple.py
@@ -1,15 +1,32 @@
-continuer = "oui"
+def demander_nombre():
+    while True:
+        try:
+            return float(input("Entrez un nombre : "))
+        except ValueError:
+            print("Erreur : veuillez entrer un nombre valide.")
 
-while continuer.lower() == "oui":
-    nombre: int = int(input("Saisissez un nombre: "))
+def demander_continuer():
+    while True:
+        reponse = input("Voulez-vous continuer ? (oui/non) : ").lower().strip()
+        if reponse in ["oui", "o", "yes", "y"]:
+            return True
+        elif reponse in ["non", "n", "no"]:
+            return False
+        else:
+            print("Réponse non reconnue. Merci de répondre par oui ou non.")
+
+continuer = True
+
+while continuer:
+    nombre = demander_nombre()
 
     if nombre > 0:
-        print("Nombre positif")
+        print("positif")
     elif nombre < 0:
-        print("Nombre negatif")
+        print("négatif")
     else:
-        print("Zero")
-        
-    continuer = input("Voulez-vous continuer ? (oui/non) : ")
-    
-print("Programme terminé")
+        print("zéro")
+
+    continuer = demander_continuer()
+
+print("Programme terminé.")
